42b583b86671e711cf69a8b89a4fc378b948dd3f
Paola A. Carreno <paobloom@hotmail.com>
2023-08-06 18:15:54 +0200

    change suscriber to ladybug camera, rotate incoming image

--- scripts/Bakenerkenner_ROS.py
+++ scripts/Bakenerkenner_ROS.py
@@ -5,16 +5,11 @@ import rosbag
 import cv2
 import numpy as np
 import os
-
-# import for Image handling
 from sensor_msgs.msg import Image
-#from my_anonymizer.msg import stereoimage
 
 # import for bagfile-to-cv2-transport and other way round (Image format change)
 from cv_bridge import CvBridge
 bridge = CvBridge()
-
-
 
 def detect(frame,cfg,weights,classes):
     # Load Model
@@ -23,17 +18,13 @@ def detect(frame,cfg,weights,classes):
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
     layer_names = net.getLayerNames()
-    #output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
-    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
+    output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
 
     # image dimensions 
     height, width, channels = frame.shape
 
     # Create the pipeline
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-    #print("Blob shape: ", blob.shape)
-    #print("Blob content: ", blob)
 
     net.setInput(blob)
     outs = net.forward(output_layers)
@@ -43,7 +34,7 @@ def detect(frame,cfg,weights,classes):
     class_ids = []
     confidences = []
     boxes = []
-    
+ 
     # Loop to predict plates
     for out in outs:
         for detection in out:
@@ -60,6 +51,8 @@ def detect(frame,cfg,weights,classes):
                 # Rectangle coordinates
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
+                
+                print("[x, y, w, h]:", x, y, w, h)
 
                 boxes.append([x, y, w, h])
                 confidences.append(float(confidence))
@@ -84,7 +77,6 @@ def detect(frame,cfg,weights,classes):
     
     return frame
 
-
 def Rectified_Image_callback(data):
 
     # Stereokamera Image abonieren
@@ -92,35 +84,28 @@ def Rectified_Image_callback(data):
     # Image von Rosformat zu OpenCV-format konvertieren
     frame = bridge.imgmsg_to_cv2(frame, desired_encoding="bgr8")
 
-    #########################################################
-    ############ Hier anonymisieren #########################
-    #########################################################
+    img_90 = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
-    frame_copy = detect(frame,cfg,weights,classes)
+    frame_copy = detect(img_90,cfg,weights,classes)
 
-    #########################################################
-    #########################################################
     # frame zurück zu ROS-Image konvertieren
     frame_copy = bridge.cv2_to_imgmsg(frame_copy, encoding="bgr8")
 
     # Frame in ROS publishen
     pub.publish(frame_copy)
 
+
 if __name__ == '__main__':
     ## Load path to model config ##
     dirname = os.path.dirname(__file__)
-    #cfg = os.path.join('/media/eddy-labor/MySSD/bakenerkenner/workspace1/src/yolov4-for-darknet_ros/darknet_ros/darknet_ros/yolo_network_config/cfg/yolov4-tiny-custom.cfg')
-    #cfg = os.path.join('/media/psf/CJ1/catkin_ws/src/darknet_ros/darknet_ros/yolo_network_config/cfg/yolov4-tiny-custom.cfg')
-    cfg = os.path.join('/media/psf/CJ1/workspace1/src/yolov4-for-darknet_ros/darknet_ros/darknet_ros/yolo_network_config/cfg/yolov4-tiny-custom.cfg')
-    #weights = os.path.join('/media/eddy-labor/MySSD/bakenerkenner/workspace1/src/yolov4-for-darknet_ros/darknet_ros/darknet_ros/yolo_network_config/weights/yolov4-tiny-custom_best.weights')
-    #weights = os.path.join('/media/psf/CJ1/catkin_ws/src/darknet_ros/darknet_ros/yolo_network_config/weights/yolov4-tiny-custom_best.weights')
-    weights = os.path.join('/media/psf/CJ1/workspace1/src/yolov4-for-darknet_ros/darknet_ros/darknet_ros/yolo_network_config/weights/yolov4-tiny-custom_best.weights')
+    cfg = os.path.join('/media/psf/CJ1/catkin_ws/src/darknet_ros/darknet_ros/yolo_network_config/cfg/yolov4-tiny-custom.cfg')
+    weights = os.path.join('/media/psf/CJ1/catkin_ws/src/darknet_ros/darknet_ros/yolo_network_config/weights/yolov4-tiny-custom_best.weights')
     classes = ['bake', 'bake2']
     #Node initialisieren
     rospy.init_node("Bakenerkenner")
     # Publisher & Subscriber erstellen
     pub = rospy.Publisher('/Bakenerkenner', Image, queue_size=1)
-    sub = rospy.Subscriber("/left_cam_node/image_raw", Image, callback=Rectified_Image_callback)
+    sub = rospy.Subscriber("/camera/image_raw", Image, callback=Rectified_Image_callback)
     #Console info
     rospy.loginfo("Publisher & Subscriber has been started.")
     rospy.loginfo("Node has been started")
