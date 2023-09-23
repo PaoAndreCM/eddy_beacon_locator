97c7ae26b4de0e184ff73d78b9cb4916a81b8881
jesm86 <jeromeasmith86@gmail.com>
2023-09-23 18:23:56 +0200

    bakenerkenner_ros.py detection functionality now in detect_beacon module. splitting of images in main module now. gps data and bound box position stored in beacon obj

--- scripts/detect_beacon.py
+++ scripts/detect_beacon.py
@@ -1,10 +1,72 @@
 from beacon import Beacon
+import cv2
+import numpy as np
 
-listOfBeacons = []
+def objDetection(frame, cfg, weights, classes, image_part_number):
 
-def objDetection(image):
-    #... add object detection
+    listOfBeacons = []
+    # Load Model
+    # Give the configuration and weight files for the model and load the network.
+    net = cv2.dnn.readNet(weights,cfg)
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
 
-    listOfBeacons.append(beacon.Beacon(1, None)) #dummy beacon type 1, no bounding box
-    listOfBeacons.append(beacon.Beacon(2, None)) #dummy beacon type 2, no bounding box
+    # image dimensions 
+    height, width, channels = frame.shape
+
+    # Create the pipeline
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    cropped_plate = []
+    crop_rect = []
+    class_ids = []
+    confidences = []
+    boxes = []
+ 
+    # Loop to predict plates
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                
+                #print("[x, y, w, h]:", x, y, w, h)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+                listOfBeacons.append(Beacon("Dummy", [x,y], image_part_number)) # insert beacon type/class later
+
+    #Detected plates put inside the image
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    for i in range(len(boxes)):
+        if i in indexes.flatten():
+            x, y, w, h = boxes[i]
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
+
+        # if ShowNumbersOfPlate:
+        #     crop_rect.append([x,y,w,h])
+        #     #Cropped detected rectangle
+        #     crop = frame[y:y+h, x:x+w]
+        #     cropped_plate.append(crop)
+        #     no_of_detected_plates = len(crop_rect)
+        #     print(no_of_detected_plates)
+        #     confidences = confidences[-no_of_detected_plates:]
+        #     print(confidence)
+
     return listOfBeacons
