e3673c2c99c89f30c35e4f0bcefd232157b410b1
jesm86 <jeromeasmith86@gmail.com>
2023-10-21 22:55:22 +0200

    :

--- scripts/beacon.py
+++ scripts/beacon.py
@@ -1,16 +1,21 @@
+import json
+
 class Beacon:
-    def __init__(self, type, boundingBoxPos, angle, distance, img_part_nr):
-        self.type            = type
-        self.boundindBoxPos  = boundingBoxPos
-        self.car_gps         = []
-        self.image_part_nr   = img_part_nr
-        self.beacon_gps      = []
-        self.beacon_angle = angle
-        self.beacon_distance = distance
- 
+    def __init__(self, type, boundingBoxPos, angle, distance):
+        self.type             = type
+        self.boundindBoxPos   = boundingBoxPos
+        self.car_gps          = []
+        #self.image_part_nr   = img_part_nr
+        self.beacon_gps       = []
+        self.beacon_angle     = angle
+        self.beacon_distance  = distance
+        self.confidence       = None
+        self.geojson_String   = None
+
     def setCarGPS(self, gps):
         self.car_gps = gps
 
+        """
         # debugging - remove later
         print("image part in which beacon was found:")
         print(self.image_part_nr)
@@ -26,6 +31,7 @@ class Beacon:
         print(self.beacon_distance)
         print()
         print()
+        """
 
     def getCarGps(self): 
         return self.car_gps
@@ -36,15 +42,21 @@ class Beacon:
     def getBoundingBox(self):
         return self.boundindBoxPos
     
-    def getBeaconData(self):
-        return [self.type, self.beaconLatitude, self.beaconLongitude]
+    #def getBeaconData(self):
+    #    return [self.type, self.beaconLatitude, self.beaconLongitude]
 
     def getBeaconAngle(self):
-        return self._beacon_angle
+        return self.beacon_angle
     
     def getBeaconDistance(self):
         return self.beacon_distance
         
     def SetBeaconAngle(self, angle):
         self._beacon_angle = angle
+    
+    def SetConfidence(self, confidence):
+        self.confidence = confidence
+
+    def getBeaconData(self):
+        return [self.type, self.beacon_gps[0], self.beacon_gps[1], self.confidence]
         
--- scripts/position_calculation.py
+++ scripts/position_calculation.py
@@ -1,20 +1,31 @@
-from math import asin, atan2, cos, degrees, radians, sin
+from math import asin, atan2, cos, degrees, radians, sin, degrees
 from beacon import Beacon
 import distance_calculation as distance
 
-def calculateLocation(carLatitude, carLongitude, d, bearing, R=6371):
+COMPASS_PHASE_SHIFT= 135
+
+def car_orientation_angle_calculation(angle_radians):
+    angle_degrees = degrees(angle_radians) % 360
+    if angle_degrees < 0:
+        angle_degrees += 360
+    elif angle_degrees == 360:
+        angle_degrees = 0
+    return angle_degrees + COMPASS_PHASE_SHIFT
+
+def calculateLocation(carLatitude, carLongitude, d, beacon_angle, z_rot, R=6371):
     """
     carLatitude: initial latitude, in degrees
     carLongitude: initial longitude, in degrees
     d: distance from car to beacon (in km)
-    bearing: (true) heading in degrees
+    z_rot: rotation of the car w.r.t. to z axis (radians)
     R: optional radius of sphere, defaults to mean radius of earth
 
     Returns new lat/lon coordinate {d}km from initial, in degrees
     """
-    lat = radians(carLatitude)
+    lat = radians(carLatitude) 
     lon = radians(carLongitude)
-    a = radians(bearing)
+    bearing = car_orientation_angle_calculation(z_rot)
+    a = radians(bearing + beacon_angle)
     beaconLat = asin(sin(lat) * cos(d/R) + cos(lat) * sin(d/R) * cos(a))
     beaconLon = lon + atan2(
         sin(a) * sin(d/R) * cos(lat),
@@ -22,6 +33,7 @@ def calculateLocation(carLatitude, carLongitude, d, bearing, R=6371):
     )
     return (degrees(beaconLat), degrees(beaconLon))
 
+"""
 def calculatePosition(currentBeacon:Beacon):
     angle = Beacon.getBeaconAngle
     distanceFromCar = distance.calculate_distance(currentBeacon)
@@ -30,4 +42,4 @@ def calculatePosition(currentBeacon:Beacon):
     lon = currentBeacon.carLongitude
     
     beaconLatitud, beaconLongitud = calculateLocation(lat, lon, distanceFromCar, angle)
-    currentBeacon.setGNSS(beaconLatitud, beaconLongitud)
+    currentBeacon.setGNSS(beaconLatitud, beaconLongitud)"""
